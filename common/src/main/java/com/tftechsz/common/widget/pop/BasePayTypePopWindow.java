20d8bc0d7d19746c349010a22af05f19577bc425
jeff <jefflin233@gmail.com>
2022-06-13 12:24:52 +0800

    提现支付方式

---
+++ common/src/main/java/com/tftechsz/common/widget/pop/BasePayTypePopWindow.java
@@ -0,0 +1,178 @@
+package com.tftechsz.common.widget.pop;
+
+import android.app.Activity;
+import android.graphics.Typeface;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.databinding.DataBindingUtil;
+
+import com.alibaba.android.arouter.launcher.ARouter;
+import com.netease.nim.uikit.common.ConfigInfo;
+import com.tftechsz.common.R;
+import com.tftechsz.common.base.BaseApplication;
+import com.tftechsz.common.databinding.PopBasePayTypeBinding;
+import com.tftechsz.common.iservice.UserProviderService;
+import com.tftechsz.common.utils.AddSpaceTextWatcher;
+import com.tftechsz.common.utils.ToastUtil;
+import com.tftechsz.common.utils.Utils;
+
+import razerdp.util.KeyboardUtils;
+
+/**
+ * 包 名 : com.tftechsz.mine.widget.pop
+ * 描 述 : 填写支付宝提现方式
+ */
+public class BasePayTypePopWindow extends BaseBottomPop {
+
+    private PopBasePayTypeBinding mBind;
+    UserProviderService userService;
+    private final ConfigInfo mConfigInfo;
+    private String number, name, card, phone;
+
+    public BasePayTypePopWindow(Activity context) {
+        super(context);
+        userService = ARouter.getInstance().navigation(UserProviderService.class);
+        mConfigInfo = userService.getConfigInfo();
+        initUI();
+    }
+
+    public void setInfo(String number, String name, String card, String phone) {
+        this.number = number;
+        this.name = name;
+        this.card = card;
+        this.phone = phone;
+        if (mBind != null) {
+            mBind.etNumber.setText(number);
+            mBind.etName.setText(name);
+            mBind.etCard.setText(card);
+            mBind.etPhone.setText(phone);
+        }
+    }
+
+    @Override
+    protected View createPopupById() {
+        View view = createPopupById(R.layout.pop_base_pay_type);
+        mBind = DataBindingUtil.bind(view);
+        return view;
+    }
+
+    private void initUI() {
+        setInfo("", "", "", "");
+        mBind.tvClosePop.setOnClickListener(v -> dismiss());
+        mBind.tvConfirm.setOnClickListener((View.OnClickListener) v -> {
+            String number = Utils.getText(mBind.etNumber);
+            String name = Utils.getText(mBind.etName);
+            String card = Utils.getText(mBind.etCard).replace(" ", "");
+            String phone = Utils.getText(mBind.etPhone).replace(" ", "");
+            if (TextUtils.isEmpty(number)) {
+                Utils.setFocus(mBind.etNumber);
+                toastTip("请输入你的支付宝账号");
+                return;
+            }
+            if (TextUtils.isEmpty(name)) {
+                Utils.setFocus(mBind.etName);
+                toastTip("请输入你的真实姓名");
+                return;
+            }
+            if (TextUtils.isEmpty(card)) {
+                Utils.setFocus(mBind.etCard);
+                toastTip("请输入你的身份证号码");
+                return;
+            }
+            if (TextUtils.isEmpty(phone)) {
+                Utils.setFocus(mBind.etPhone);
+                toastTip("请输入你的手机号码");
+                return;
+            }
+            if (!Utils.regexEmail(mConfigInfo, number) && !Utils.regexTell(mConfigInfo, number)) {
+                Utils.setFocus(mBind.etNumber);
+                toastTip("支付宝账号格式错误");
+                return;
+            }
+            if (!Utils.regexChineseName(mConfigInfo, name)) {
+                Utils.setFocus(mBind.etName);
+                toastTip("姓名格式错误");
+                return;
+            }
+            if (!Utils.regexIdCard(mConfigInfo, card)) {
+                Utils.setFocus(mBind.etCard);
+                toastTip("身份证格式错误");
+                return;
+            }
+            if (!Utils.regexTell(mConfigInfo, phone)) {
+                Utils.setFocus(mBind.etPhone);
+                toastTip("手机号格式错误");
+                return;
+            }
+            KeyboardUtils.close(mBind.etName);
+            KeyboardUtils.close(mBind.etNumber);
+            KeyboardUtils.close(mBind.etCard);
+            KeyboardUtils.close(mBind.etPhone);
+            listener.onConfirm(number, card, name, phone);
+            dismiss();
+        });
+
+        mBind.etNumber.addTextChangedListener(new MyInputFilter(mBind.etNumber));
+        mBind.etCard.addTextChangedListener(new MyInputFilter(mBind.etCard));
+        mBind.etName.addTextChangedListener(new MyInputFilter(mBind.etName));
+        mBind.etPhone.addTextChangedListener(new MyInputFilter(mBind.etPhone));
+        new AddSpaceTextWatcher(mBind.etPhone, AddSpaceTextWatcher.SpaceType.mobilePhoneNumberType);
+        new AddSpaceTextWatcher(mBind.etCard, AddSpaceTextWatcher.SpaceType.IDCardNumberType);
+    }
+
+    private void editListener() {
+        mBind.tvConfirm.setEnabled(!TextUtils.isEmpty(Utils.getText(mBind.etNumber))
+                && !TextUtils.isEmpty(Utils.getText(mBind.etCard))
+                && !TextUtils.isEmpty(Utils.getText(mBind.etName))
+                && !TextUtils.isEmpty(Utils.getText(mBind.etPhone)));
+    }
+
+    private class MyInputFilter implements TextWatcher {
+        private final EditText mEditText;
+        Typeface normal = Typeface.defaultFromStyle(Typeface.NORMAL);
+        Typeface bold = Typeface.defaultFromStyle(Typeface.BOLD);
+
+        public MyInputFilter(EditText editText) {
+            mEditText = editText;
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            editListener();
+            if (TextUtils.isEmpty(Utils.getText(mEditText))) {
+                mEditText.setTypeface(normal);
+            } else {
+                mEditText.setTypeface(bold);
+
+            }
+        }
+    }
+
+    public interface OnConfirmListener {
+        void onConfirm(String number, String card, String name, String phone);
+    }
+
+    public OnConfirmListener listener;
+
+    public void addOnConfirmListener(OnConfirmListener listener) {
+        this.listener = listener;
+    }
+
+    public void toastTip(final String msg) {
+        ToastUtil.showToast(BaseApplication.getInstance(), msg);
+    }
+}
--- mine/src/main/java/com/tftechsz/mine/mvp/ui/activity/WithdrawActivity.java
+++ mine/src/main/java/com/tftechsz/mine/mvp/ui/activity/WithdrawActivity.java
@@ -2,13 +2,9 @@ package com.tftechsz.mine.mvp.ui.activity;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.text.Editable;
 import android.text.TextUtils;
-import android.text.TextWatcher;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Autowired;
@@ -17,13 +13,13 @@ import com.netease.nim.uikit.common.ConfigInfo;
 import com.tftechsz.common.Constants;
 import com.tftechsz.common.base.BaseMvpActivity;
 import com.tftechsz.common.bus.RxBus;
+import com.tftechsz.common.entity.WithdrawReq;
 import com.tftechsz.common.event.CommonEvent;
 import com.tftechsz.common.iservice.UserProviderService;
-import com.tftechsz.common.utils.AddSpaceTextWatcher;
 import com.tftechsz.common.utils.Utils;
+import com.tftechsz.common.widget.pop.BasePayTypePopWindow;
 import com.tftechsz.mine.R;
 import com.tftechsz.mine.entity.dto.ShopInfoDto;
-import com.tftechsz.common.entity.WithdrawReq;
 import com.tftechsz.mine.mvp.IView.IWithdrawView;
 import com.tftechsz.mine.mvp.presenter.WithdrawPresenter;
 
@@ -33,13 +29,19 @@ public class WithdrawActivity extends BaseMvpActivity<IWithdrawView, WithdrawPre
     private static final String EXTRA_COIN = "extra_coin";
     private ShopInfoDto mData;
     private TextView mTvCoinNumber;
-    private EditText mEtNumber, mEtName, mEtCard, mEtPhone;
     private TextView mTvIntegral;   //扣除积分
     private TextView mTvExchange;
     private TextView mTvWithdrawTip;
+    private View tvEditPayInfo;
+    private View tvAddPayInfo;
+    private View clPayInfo;
+    private TextView tvPayName, tvPayNumber;
     @Autowired
     UserProviderService service;
     private ConfigInfo mConfigInfo;
+
+    private String number, name, card, phone;
+    private BasePayTypePopWindow popWindow;
 
     public static void startActivity(Context context, ShopInfoDto coin) {
         Intent intent = new Intent(context, WithdrawActivity.class);
@@ -59,13 +61,14 @@ public class WithdrawActivity extends BaseMvpActivity<IWithdrawView, WithdrawPre
                 .setTitle("确认兑换")
                 .build();
         mTvCoinNumber = findViewById(R.id.tv_coin_num);
-        mEtNumber = findViewById(R.id.et_number);
-        mEtCard = findViewById(R.id.et_card);
-        mEtName = findViewById(R.id.et_name);
-        mEtPhone = findViewById(R.id.et_phone);
         mTvIntegral = findViewById(R.id.tv_integral);
         mTvExchange = findViewById(R.id.tv_exchange);
         mTvWithdrawTip = findViewById(R.id.tv_withdraw_tip);
+        tvEditPayInfo = findViewById(R.id.tv_pay_info_edit);
+        tvAddPayInfo = findViewById(R.id.tv_add_pay_type);
+        clPayInfo = findViewById(R.id.cl_pay_ali_add_info);
+        tvPayName = findViewById(R.id.tv_pay_name);
+        tvPayNumber = findViewById(R.id.tv_pay_number);
         Utils.setFocus(mTvCoinNumber);
         initListener();
         initRxBus();
@@ -86,22 +89,35 @@ public class WithdrawActivity extends BaseMvpActivity<IWithdrawView, WithdrawPre
 
     private void initListener() {
         mTvExchange.setOnClickListener(this);  //兑换
-        mEtNumber.addTextChangedListener(new MyInputFilter(mEtNumber));
-        mEtCard.addTextChangedListener(new MyInputFilter(mEtCard));
-        mEtName.addTextChangedListener(new MyInputFilter(mEtName));
-        mEtPhone.addTextChangedListener(new MyInputFilter(mEtPhone));
-        new AddSpaceTextWatcher(mEtPhone, AddSpaceTextWatcher.SpaceType.mobilePhoneNumberType);
-        new AddSpaceTextWatcher(mEtCard, AddSpaceTextWatcher.SpaceType.IDCardNumberType);
+        tvEditPayInfo.setOnClickListener(this);
+        tvAddPayInfo.setOnClickListener(this);
     }
 
-    private void editListener() {
-        if (TextUtils.isEmpty(Utils.getText(mEtNumber))
-                || TextUtils.isEmpty(Utils.getText(mEtCard))
-                || TextUtils.isEmpty(Utils.getText(mEtName))
-                || TextUtils.isEmpty(Utils.getText(mEtPhone))) {
-            mTvExchange.setBackgroundResource(R.drawable.bg_gray_ee);
+    private void showPayInfoPop() {
+        if (popWindow == null) {
+            popWindow = new BasePayTypePopWindow(this);
+            popWindow.addOnConfirmListener(this::setPayWayInfo);
+        }
+        popWindow.setInfo(number, name, card, phone);
+        popWindow.showPopupWindow();
+    }
+
+    private void setPayWayInfo(String number, String card, String name, String phone) {
+        if (!TextUtils.isEmpty(number)
+                && !TextUtils.isEmpty(card)
+                && !TextUtils.isEmpty(name)
+                && !TextUtils.isEmpty(phone)) {
+            WithdrawActivity.this.number = number;
+            WithdrawActivity.this.card = card;
+            WithdrawActivity.this.name = name;
+            WithdrawActivity.this.phone = phone;
+            tvPayName.setText(name);
+            tvPayNumber.setText(number);
+            tvAddPayInfo.setVisibility(View.GONE);
+            clPayInfo.setVisibility(View.VISIBLE);
+            mTvExchange.setEnabled(true);
         } else {
-            mTvExchange.setBackgroundResource(R.drawable.bg_orange_radius);
+            mTvExchange.setEnabled(false);
         }
     }
 
@@ -127,51 +143,21 @@ public class WithdrawActivity extends BaseMvpActivity<IWithdrawView, WithdrawPre
     @Override
     public void onClick(View v) {
         int id = v.getId();
-        if (null == mData)
-            return;
-        if (id == R.id.tv_exchange) {   //兑换
-            String number = Utils.getText(mEtNumber);
-            String name = Utils.getText(mEtName);
-            String card = Utils.getText(mEtCard).replace(" ", "");
-            String phone = Utils.getText(mEtPhone).replace(" ", "");
+        if (id == R.id.tv_pay_info_edit || id == R.id.tv_add_pay_type) {
+            showPayInfoPop();
+        } else if (id == R.id.tv_exchange) {   //兑换
+            if (null == mData)
+                return;
             if (TextUtils.isEmpty(number)) {
-                Utils.setFocus(mEtNumber);
-                toastTip("请输入你的支付宝账号");
                 return;
             }
             if (TextUtils.isEmpty(name)) {
-                Utils.setFocus(mEtName);
-                toastTip("请输入你的真实姓名");
                 return;
             }
             if (TextUtils.isEmpty(card)) {
-                Utils.setFocus(mEtCard);
-                toastTip("请输入你的身份证号码");
                 return;
             }
             if (TextUtils.isEmpty(phone)) {
-                Utils.setFocus(mEtPhone);
-                toastTip("请输入你的手机号码");
-                return;
-            }
-            if (!Utils.regexEmail(mConfigInfo, number) && !Utils.regexTell(mConfigInfo, number)) {
-                Utils.setFocus(mEtNumber);
-                toastTip("支付宝账号格式错误");
-                return;
-            }
-            if (!Utils.regexChineseName(mConfigInfo, name)) {
-                Utils.setFocus(mEtName);
-                toastTip("姓名格式错误");
-                return;
-            }
-            if (!Utils.regexIdCard(mConfigInfo, card)) {
-                Utils.setFocus(mEtCard);
-                toastTip("身份证格式错误");
-                return;
-            }
-            if (!Utils.regexTell(mConfigInfo, phone)) {
-                Utils.setFocus(mEtPhone);
-                toastTip("手机号格式错误");
                 return;
             }
             p.withdraw(mData.id, name, number, card, phone);
@@ -185,40 +171,6 @@ public class WithdrawActivity extends BaseMvpActivity<IWithdrawView, WithdrawPre
 
     @Override
     public void withdrawWaySuccess(WithdrawReq.Withdraw msg) {
-        mEtName.setText(msg.name);
-        mEtCard.setText(msg.identity);
-        mEtNumber.setText(msg.account);
-        mEtPhone.setText(msg.phone);
-    }
-
-    private class MyInputFilter implements TextWatcher {
-        private final EditText mEditText;
-        Typeface normal = Typeface.defaultFromStyle(Typeface.NORMAL);
-        Typeface bold = Typeface.defaultFromStyle(Typeface.BOLD);
-
-        public MyInputFilter(EditText editText) {
-            mEditText = editText;
-        }
-
-        @Override
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-        }
-
-        @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-        }
-
-        @Override
-        public void afterTextChanged(Editable s) {
-            editListener();
-            if (TextUtils.isEmpty(Utils.getText(mEditText))) {
-                mEditText.setTypeface(normal);
-            } else {
-                mEditText.setTypeface(bold);
-
-            }
-        }
+        setPayWayInfo(msg.account, msg.identity, msg.name, msg.phone);
     }
 }
