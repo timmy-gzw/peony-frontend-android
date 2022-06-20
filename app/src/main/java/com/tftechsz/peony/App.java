b91132a74f7db882238b6a7bea00fd3d198d750b
jeff <jefflin233@gmail.com>
2022-06-20 16:27:02 +0800

    set default refreshHeader

--- app/src/main/java/com/tftechsz/peony/App.java
+++ app/src/main/java/com/tftechsz/peony/App.java
@@ -4,18 +4,18 @@ package com.tftechsz.peony;
 import android.content.Context;
 
 import com.scwang.smart.refresh.footer.ClassicsFooter;
+import com.scwang.smart.refresh.header.ClassicsHeader;
 import com.scwang.smart.refresh.layout.SmartRefreshLayout;
 import com.scwang.smart.refresh.layout.api.RefreshFooter;
 import com.scwang.smart.refresh.layout.api.RefreshHeader;
 import com.scwang.smart.refresh.layout.api.RefreshLayout;
 import com.scwang.smart.refresh.layout.listener.DefaultRefreshFooterCreator;
 import com.scwang.smart.refresh.layout.listener.DefaultRefreshHeaderCreator;
-import com.umeng.socialize.PlatformConfig;
 import com.tftechsz.common.Constants;
 import com.tftechsz.common.base.BaseApplication;
-import com.tftechsz.common.refresh.MyClassicsHeader;
-import timber.log.Timber;
+import com.umeng.socialize.PlatformConfig;
 
+import timber.log.Timber;
 
 
 public class App extends BaseApplication {
@@ -27,8 +27,9 @@ public class App extends BaseApplication {
         SmartRefreshLayout.setDefaultRefreshHeaderCreator(new DefaultRefreshHeaderCreator() {
             @Override
             public RefreshHeader createRefreshHeader(Context context, RefreshLayout layout) {
-                layout.setPrimaryColorsId(R.color.white,R.color.color_normal);//全局设置主题颜色
-                return new MyClassicsHeader(context);//.setTimeFormat(new DynamicTimeFormat("更新于 %s"));//指定为经典Header，默认是 贝塞尔雷达Header
+                layout.setPrimaryColorsId(R.color.white, R.color.color_normal);//全局设置主题颜色
+//                return new MyClassicsHeader(context);//.setTimeFormat(new DynamicTimeFormat("更新于 %s"));//指定为经典Header，默认是 贝塞尔雷达Header
+                return new ClassicsHeader(context);//.setTimeFormat(new DynamicTimeFormat("更新于 %s"));//指定为经典Header，默认是 贝塞尔雷达Header
             }
         });
         //设置全局的Footer构建器
