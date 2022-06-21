fbc45ed89c8bddfc1b6cc832a8c0ae84c5d2c730
jeff <jefflin233@gmail.com>
2022-06-21 20:23:34 +0800

    土豪&魅力等级弹窗

--- common/src/main/java/com/tftechsz/common/widget/pop/UserLevelUpPop.java
+++ common/src/main/java/com/tftechsz/common/widget/pop/UserLevelUpPop.java
@@ -2,11 +2,10 @@ package com.tftechsz.common.widget.pop;
 
 import android.content.Context;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.core.content.ContextCompat;
 
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.netease.nim.uikit.common.ChatMsg;
@@ -19,9 +18,9 @@ import com.tftechsz.common.utils.GlideUtils;
  * 描 述 : TODO
  */
 public class UserLevelUpPop extends BaseCenterPop {
-    private ImageView ivRotate, rightIcon;
+    private ImageView ivRotate, ivLevelBg;
     private View iconStroke;
-    private TextView levelCard, levelTips;
+    private TextView levelTips,tvLevelTitle;
     private final UserProviderService service;
 
     public UserLevelUpPop(Context context) {
@@ -34,10 +33,10 @@ public class UserLevelUpPop extends BaseCenterPop {
         ivRotate = findViewById(R.id.iv_rotate);
         ivRotate.setVisibility(View.GONE);
         ImageView icon = findViewById(R.id.icon);
-        rightIcon = findViewById(R.id.right_icon);
         iconStroke = findViewById(R.id.icon_stroke);
-        levelCard = findViewById(R.id.level_card);
         levelTips = findViewById(R.id.level_tips);
+        ivLevelBg = findViewById(R.id.level_bg);
+        tvLevelTitle = findViewById(R.id.tv_level_cont);
         GlideUtils.loadRouteImage(getContext(), icon, service.getUserInfo().getIcon());
         setOnDismissListener(new OnDismissListener() {
             @Override
@@ -55,24 +54,16 @@ public class UserLevelUpPop extends BaseCenterPop {
 
     public void setRotate() {
         ivRotate.setVisibility(View.VISIBLE);
-        RotateAnimation rotate = new RotateAnimation(0f, 360f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-        LinearInterpolator lin = new LinearInterpolator();
-        rotate.setInterpolator(lin);
-        rotate.setDuration(5 * 1000);
-        rotate.setRepeatCount(-1);
-        rotate.setFillAfter(true);
-        rotate.setStartOffset(10);
-        ivRotate.setAnimation(rotate);
     }
 
     public void setData(ChatMsg.UserLevelUp data) {
-        levelCard.setText(data.user_title);
         levelTips.setText(data.user_tips);
         if (data.user_type.equals("charm")) {
-            rightIcon.setImageResource(R.mipmap.mine_ic_charm_love);
-            levelCard.setBackgroundResource(R.mipmap.mine_bg_charm_content);
+            ivRotate.setBackgroundResource(R.mipmap.peony_qd_bbg_icon_2);
+            ivLevelBg.setImageResource(R.mipmap.peony_bpqtt_img_2);
             iconStroke.setBackgroundResource(R.drawable.shape_meili_icon_bg_pop);
+            levelTips.setTextColor(ContextCompat.getColor(mContext, R.color.c_cc3d3d));
+            tvLevelTitle.setTextColor(ContextCompat.getColor(mContext, R.color.c_cc3d3d));
         }
-
     }
 }
