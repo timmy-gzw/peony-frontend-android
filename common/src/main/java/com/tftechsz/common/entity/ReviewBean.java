93256e150e0ce44b6efee374ca8e9dfb736d003c
jeff <jefflin233@gmail.com>
2022-07-06 15:19:37 +0800

    review status

--- common/src/main/java/com/tftechsz/common/entity/ReviewBean.java
+++ common/src/main/java/com/tftechsz/common/entity/ReviewBean.java
@@ -1,7 +1,7 @@
 package com.tftechsz.common.entity;
 
 public class ReviewBean {
-    private boolean r;//是否正在审核
+    private boolean r;//是否正在审核 true:审核中
     private boolean srl;//是否获取定位权限 false：不获取  true：获取权限
 
     public boolean isR() {
