e2de97eed87fd61e9aeb3a542321d4316927b387
jeff <jefflin233@gmail.com>
2022-08-10 16:48:33 +0800

    im content color

--- im/src/main/java/com/tftechsz/im/adapter/MessageAdapter.java
+++ im/src/main/java/com/tftechsz/im/adapter/MessageAdapter.java
@@ -31,9 +31,6 @@ import com.netease.nimlib.sdk.msg.model.IMMessage;
 import com.netease.nimlib.sdk.msg.model.QueryDirectionEnum;
 import com.netease.nimlib.sdk.uinfo.UserService;
 import com.netease.nimlib.sdk.uinfo.model.NimUserInfo;
-import com.tftechsz.im.R;
-import com.tftechsz.im.api.ChatApiService;
-import com.tftechsz.im.model.ContactInfo;
 import com.tftechsz.common.Constants;
 import com.tftechsz.common.http.RetrofitManager;
 import com.tftechsz.common.iservice.UserProviderService;
@@ -41,6 +38,9 @@ import com.tftechsz.common.utils.CommonUtil;
 import com.tftechsz.common.utils.StringUtils;
 import com.tftechsz.common.utils.Utils;
 import com.tftechsz.common.widget.chat.ChatTimeUtils;
+import com.tftechsz.im.R;
+import com.tftechsz.im.api.ChatApiService;
+import com.tftechsz.im.model.ContactInfo;
 
 import java.util.List;
 import java.util.Map;
@@ -54,7 +54,7 @@ public class MessageAdapter extends BaseQuickAdapter<ContactInfo, BaseViewHolder
     private final UserProviderService service;
     private final int mType;   //0:消息  1：密友  2:聊天房私信
     public int mIntimacyNum;
-    private CompositeDisposable mCompositeDisposable;
+    private final CompositeDisposable mCompositeDisposable;
 
     public MessageAdapter(@Nullable List<ContactInfo> data, int type) {
         super(R.layout.item_message, data);
@@ -79,7 +79,7 @@ public class MessageAdapter extends BaseQuickAdapter<ContactInfo, BaseViewHolder
         AvatarVipFrameView ivAvatar = helper.getView(R.id.iv_avatar);
         ivAvatar.setOnline(mType == 1 && item.is_online);
         //是否在线
-        helper.setVisible(R.id.iv_online,item.is_online);
+        helper.setVisible(R.id.iv_online, item.is_online);
         //文本颜色
         helper.setTextColor(R.id.tv_content, getContext().getResources().getColor(R.color.color_light_font));
         if (StickTopCache.isStickTop(item)) {  //y已经置顶
@@ -146,9 +146,7 @@ public class MessageAdapter extends BaseQuickAdapter<ContactInfo, BaseViewHolder
                 ivOffice.setImageResource(R.mipmap.ic_official);
                 ivAvatar.setAvatar(R.drawable.chat_ic_customer_service_secretary);
                 CommonUtil.setUserName(tvName, "客服小秘书", false);
-                if(Utils.isHeart()){
-                    tvContent.setTextColor(Utils.getColor(R.color.black));
-                }
+                tvContent.setTextColor(Utils.getColor(R.color.c_im_content));
             } else {
                 if (mType == 2) {
                     ivAvatar.setPartyAvatar(item.getContactId());
@@ -207,7 +205,7 @@ public class MessageAdapter extends BaseQuickAdapter<ContactInfo, BaseViewHolder
                                     }
                                 } else {
                                     String content = message.getContent();
-                                    if(String.valueOf(service.getUserId()).equals(message.getFromAccount()) && content.equals("[对方发来搭讪红包]")){
+                                    if (String.valueOf(service.getUserId()).equals(message.getFromAccount()) && content.equals("[对方发来搭讪红包]")) {
                                         content = "[发出搭讪红包]";
                                     }
                                     item.content = setContent(textView, message, content);
