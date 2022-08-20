b93e4ec013f399e25529e7992f23128ff257409e
dqm <dengqianming@1580752867452302.onaliyun.com>
2022-08-20 16:14:31 +0800

    imei校验

--- common/src/main/java/com/tftechsz/common/utils/AppUtils.java
+++ common/src/main/java/com/tftechsz/common/utils/AppUtils.java
@@ -872,11 +872,43 @@ public class AppUtils {
                 savaString(LOACL_IMEI, imei);
                 saveDeviceIdType(LOACL_IMEI, imei);
             }
+            if (TextUtils.isEmpty(imei) || imei.startsWith("00000") || imei.equals("0")) {
+               imei = "";
+            }
+            if (!isCorrectImei(imei))
+                imei = "";
             return imei;
         } catch (Exception e) {
 
         }
         return null;
+    }
+
+
+    /**
+     * 判断IMEI号是否合法，合法返回true，不合法返回false
+     */
+    public static Boolean isCorrectImei(String imei) {
+        final int imeiLength = 15;
+        if (imei.length() == imeiLength) {
+            int check = Integer.parseInt(imei.substring(14));
+            imei = imei.substring(0, 14);
+            char[] imeiChar = imei.toCharArray();
+            int resultInt = 0;
+            for (int i = 0; i < imeiChar.length; i++) {
+                int a = Integer.parseInt(String.valueOf(imeiChar[i]));
+                i++;
+                final int temp = Integer.parseInt(String.valueOf(imeiChar[i])) * 2;
+                final int b = temp < 10 ? temp : temp - 9;
+                resultInt += a + b;
+            }
+            resultInt %= 10;
+            resultInt = resultInt == 0 ? 0 : 10 - resultInt;
+            if (resultInt == check) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private static String getSavaString(String key, String defValue) {
