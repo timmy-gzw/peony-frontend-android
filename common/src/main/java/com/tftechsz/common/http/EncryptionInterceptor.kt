a0450494d834001ca7d0dfba3d540c0c1f644994
jeff <jefflin233@gmail.com>
2022-10-26 14:12:12 +0800

    优化加密拦截器url取值

--- common/src/main/java/com/tftechsz/common/http/EncryptionInterceptor.kt
+++ common/src/main/java/com/tftechsz/common/http/EncryptionInterceptor.kt
@@ -2,7 +2,6 @@ package com.tftechsz.common.http
 
 import com.alibaba.android.arouter.launcher.ARouter
 import com.blankj.utilcode.util.GsonUtils
-import com.blankj.utilcode.util.LogUtils
 import com.tftechsz.common.iservice.UserProviderService
 import com.tftechsz.common.utils.enc.XXTEA
 import okhttp3.Interceptor
@@ -31,38 +30,30 @@ class EncryptionInterceptor : Interceptor {
         val srcPath = srcHttpUrl.encodedPath
 
         if (!key.isNullOrBlank() && !(srcPath == "/launch" || srcPath == dstPath || srcPath.contains("/jsonConfig/"))) {
-            val srcMethod = srcRequest.method
             val srcRequestBody = srcRequest.body
             val srcContentType = srcRequestBody?.contentType()
-            val srcScheme = srcHttpUrl.scheme
-            val srcHost = srcHttpUrl.host
-            val srcPort = srcHttpUrl.port
             val srcQuery = srcHttpUrl.encodedQuery
-            val dstUrl = "$srcScheme://$srcHost:$srcPort$dstPath"
             // 拼接目标路径GET/POST /xx/yy?aa=bb&cc=dd
-            val dstPath = if (srcQuery.isNullOrBlank()) srcPath else "$srcPath?${srcQuery}"
-            val dstRequestBuilder = srcRequest.newBuilder()
-            val dstHeadersMap = HashMap<String, String>()
+            val srcPathWithQuery = if (srcQuery.isNullOrBlank()) srcPath else "$srcPath?${srcQuery}"
+            val srcHeadersMap = HashMap<String, String>()
             var dstParamsBody: String? = null
-            //读取POST body参数
-            if ("POST" == srcMethod && srcRequestBody != null) {
+            //读取 request body 内容
+            if (srcRequestBody != null) {
                 val buffer = Buffer()
                 srcRequestBody.writeTo(buffer)
                 val charset = srcContentType?.charset(StandardCharsets.UTF_8) ?: StandardCharsets.UTF_8
-                val requestParams = buffer.readString(charset)
-                dstParamsBody = requestParams
-//                LogUtils.e("--- req: POST isFormBody = ${srcRequestBody is FormBody} ---")
-//                LogUtils.e("--- req: POST body ---\n$dstParamsBody\n-----------------------------")
+                dstParamsBody = buffer.readString(charset)
+//                LogUtils.e("--- req: body ---\n$dstParamsBody\n-----------------------------")
             }
             val srcHeaderIterator = srcRequest.headers.iterator()
             while (srcHeaderIterator.hasNext()) {
                 val next = srcHeaderIterator.next()
-                dstHeadersMap[next.first] = next.second
+                srcHeadersMap[next.first] = next.second
             }
             val dstReqBodyParamsMap = HashMap<String, Any>()
-            dstReqBodyParamsMap["path"] = dstPath
-            dstReqBodyParamsMap["method"] = srcMethod
-            dstReqBodyParamsMap["headers"] = dstHeadersMap
+            dstReqBodyParamsMap["path"] = srcPathWithQuery
+            dstReqBodyParamsMap["method"] = srcRequest.method
+            dstReqBodyParamsMap["headers"] = srcHeadersMap
             dstReqBodyParamsMap["ct"] = srcContentType?.toString() ?: ""
             if (!dstParamsBody.isNullOrBlank()) {
                 dstReqBodyParamsMap["body"] = dstParamsBody
@@ -70,26 +61,27 @@ class EncryptionInterceptor : Interceptor {
 
             val dstReqBodyParams = GsonUtils.toJson(dstReqBodyParamsMap)
             val dstReqBodyParamsEnc = XXTEA.encryptToBase64String(dstReqBodyParams, key)
-            LogUtils.e("--- req: original params ---\n$dstReqBodyParams")
+//            LogUtils.e("--- req: original params ---\n$dstReqBodyParams")
 //            LogUtils.e("--- req: params with tea ---\n$dstReqBodyParamsEnc\n-----------------------------")
 
             // 构造请求体
+            val dstHttpUrl = srcHttpUrl.newBuilder().encodedPath(dstPath).query(null).build()
+//            LogUtils.e("--- HttpUrl ---\nsrcHttpUrl: $srcHttpUrl\ndstHttpUrl: $dstHttpUrl\n-----------------------------")
             val dstReqBody = dstReqBodyParamsEnc.toRequestBody("application/json".toMediaType())
-            val dstRequest = dstRequestBuilder
+            val dstRequest = srcRequest.newBuilder()
                 .removeHeader("x-auth-token")
                 .removeHeader("api-ua")
                 .removeHeader("x-user-code")
                 .removeHeader("x-anti-cheat-token")
                 .removeHeader("dit")
-                .url(dstUrl)
+                .url(dstHttpUrl)
                 .method("POST", dstReqBody)
                 .build()
 
 //            LogUtils.e("--- targetRequest ---\n$dstRequest\n-----------------------------")
             val dstResp = chain.proceed(dstRequest)
             val dstBody = dstResp.body
-            val dstRespBodyStringEnc = dstBody?.string()// base64Encode 的结果体
-            val dstRespBodyString = XXTEA.decryptBase64StringToString(dstRespBodyStringEnc, key)
+            val dstRespBodyString = XXTEA.decryptBase64StringToString(dstBody?.string(), key)
 //            LogUtils.e("--- resp: decrypted response ---\n$dstRespBodyString\n-----------------------------")
             val dstRespBody = dstRespBodyString?.toResponseBody(dstBody?.contentType())
             return dstResp.newBuilder()
